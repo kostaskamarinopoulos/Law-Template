2be11be45f22cd90460bf720b0eae5b1c0f861a4
stikas <kkamarin87@hotmail.com>
2018-07-06 00:11:21 +0100

    Fix indexes

--- index.php
+++ index.php
@@ -1,1 +1,415 @@
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Νίκος Πετρόπουλος </title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+	
+<link href="css/jcarousel.css" rel="stylesheet" />
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+<!-- ===============Navigation==============-->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Νίκος Πετρόπουλος <br> <i style="font-size:15px;">Δικηγορικό Γραφείο</i></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Υπηρεσιες</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Ποιοι Ειμαστε</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Επικοινωνια</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+<!-- ===============Header==============-->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Καλώς ήρθατε στο Δικηγορικό μας Γραφείο</div>
+                <div class="intro-heading">Μάθετε Τα Πάντα Για Εμάς</div>
+                <a href="#services" class="page-scroll btn btn-xl">Υπηρεσιες</a>
+            </div>
+        </div>
+    </header>
+
+<!-- ===============Services==============-->
+<section class ="services" id="services">
+				<div class="container">
+				<div class="row">
+
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Υπηρεσίες</h2>
+							<hr></hr>
+				</div>
+					
+					<div class="work-sec">
+						<div class="col-md-12 fil-btn" style="min-width:500px;">
+							<div class="filter col-md-1 col-sm-1 col-xs-1 wrk-title active" data-filter="all">Όλες</div>
+							<div class="filter col-md-3 col-sm-2 col-xs-3 wrk-title" data-filter=".category-1">Αστικού Ενδιαφέροντος</div>
+							<div class="filter col-md-3 col-sm-3 col-xs-3 wrk-title" data-filter=".category-2">Εμπορικού Ενδιαφέροντος</div>
+							<div class="filter col-md-3 col-sm-3 col-xs-3 wrk-title" data-filter=".category-4">Πνευματική και Βιομηχανική Ιδιοκτησία‏</div>
+							<div class="filter col-md-1 col-sm-1 col-xs-3 wrk-title lst-cld" data-filter=".category-3">Συμβόλαια</div>
+						</div>
+							<div id="Container">
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Eργατικού Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Εμπραγμάτου Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Ενοχικού Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Αυτοκινητικές Υποθέσεις</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Οικογενειακού Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Κληρονομικού Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Εμπορικού Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Ιδιωτικού Διεθνούς Δικαίου</h3>
+								</div>
+								<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+										<h3>Ακίνητα</h3>
+								</div>
+								
+								
+								<div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Κατοχύρωση και προστασία εμπορικού σήματος</h3>
+								</div>
+
+								<div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Συμβάσεις μεταφοράς νέας τεχνολογίας</h3>
+								</div>
+								
+								
+								
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Σύσταση εταιρειών</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Τροποποίηση εταιρειών</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Λύση εταιρειών</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Συμβάσεις δικαιόχρησης</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Εμπορικές συμβάσεις</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Ίδρυση υποκαταστημάτων ξένων εταιρειών</h3>
+								</div>
+								<div class="filimg mix category-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Συμβάσεις αντιπροσωπείας και διανομής</h3>
+								</div>
+								
+								
+								<div class="filimg mix category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Αγοραπωλησία</h3>
+								</div>
+								<div class="filimg mix category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Δωρέα</h3>
+								</div>
+								<div class="filimg mix category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Γονική Παροχή</h3>
+								</div>
+																<div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+										<h3>Κατοχύρωση και προστασία ονόματος χώρου (domain name)</h3>
+								</div>
+																
+							</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	<!-- ----------------End Services ---------------------->
+
+
+<!-- ===============About==============-->
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Ποιοί Είμαστε</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/1.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2002-Σήμερα</h4>
+                                    <h4 class="subheading"></h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Το δικηγορικό μας γραφείο ιδρύθηκε το έτος 2002 από τον Πετρόπουλο Νικόλαο. Έκτοτε και μέχρι σήμερα παρέχει προς τους πελάτες του υψηλού επιπέδου νομικές υπηρεσίες, σε όλους σχεδόν τους τομείς του δικαίου με ιδιαίτερη εξειδίκευση στο Εμπορικό και Αστικό δίκαιο. </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/3.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Συνεργάτες</h4>
+                                    <h4 class="subheading"></h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Διατηρούμε δίκτυο συνεργατών δικηγόρων ειδικευμένων σε όλους τους τομείς του δικαίου και σε όλες τις πόλεις της Ελλάδας, ώστε να εξασφαλίζουμε την άμεση, πλήρη και σφαιρική εξυπηρέτηση των πελατών μας.</p>
+                                </div>
+                            </div>
+                        </li>
+<!--                         <li>
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/3.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>December 2012</h4>
+                                    <h4 class="subheading">Transition to Full Service</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li> -->
+                        
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <h4>Γίνε μέρος
+                                    <br>της
+                                    <br>Ιστορίας μας</h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+<!-- ===============Contact==============-->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Επικοινωνία</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row">
+						    <div class="col-sm-6 wow animated fadeInLeft">
+                                    <div class="contact-top-one">
+										<h4>Διεύθυνση:</h4>
+											<p>Κυρίλλου Λουκάρεως 9 (1ος όροφος),<br> 
+											<span>Αθήνα</span>						
+											</p>
+									</div><br>
+									<div class="contact-top-one">
+										<h4>Τηλ. Επικοινωνίας:</h4>
+											<p>Σταθερό: 210 3633300 <br>
+											<span>Κινητό: 697 2608263</span>
+											</p>
+									</div><br>
+									<div class="contact-top-one">
+										<h4 style="font-weight: 600;">E-mail:</h4>
+										<p><a href="mailto:@gmail.com">nikospetropoulos.law@gmail.com</a></p>
+									</div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Ονομα *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" placeholder="Το Μηνυμα Σου ... *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-xl">Αποστολη</button>
+                            </div>
+                        </div>
+                    </form>					
+                    <br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.5370876447205!2d23.74934121475818!3d37.98793110764104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd52a7afa4c3%3A0x250f32cc20f0153b!2sKirillou+Loukareos+9%2C+Neapoli+Sikies+114+71%2C+Greece!5e0!3m2!1sen!2suk!4v1447691531162" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+<!-- ===============Footer==============-->
+    <footer>
+        <div class="container">
+            <div class="row">
+    	  			<span>Δικηγορικό Γραφείο Πετρόπουλος Νικος. All Rights Reserved &copy;2016 | Created by <b><a href="http://sandk-developers.blogspot.co.uk/" target="_blank">S&K Developers</a></b></span>
+
+<!--                 <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div> -->
+            </div>
+        </div>
+    </footer>    
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+	
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+	
+    	<script src="js/jquery.mixitup.js" type="text/javascript"></script>
+		    	<script src="js/owl.carousel.js"></script>
+
+	    	<script type="text/javascript">
+		    	$(function(){
+
+			// Instantiate MixItUp:
+
+			$('#Container').mixItUp();
+
+		});
+    	</script>
+    	<script>
+		    		jQuery(function ($) {
+		    // fancybox
+		    $(".fancybox").fancybox({
+		        modal: true, // disable regular nav and close buttons
+		        // add buttons helper (requires buttons helper js and css files)
+		        helpers: {
+		            buttons: {}
+		        } 
+		    });
+		    // filter selector
+		    $(".filter").on("click", function () {
+		        var $this = $(this);
+		        // if we click the active tab, do nothing
+		        if ( !$this.hasClass("active") ) {
+		            $(".filter").removeClass("active");
+		            $this.addClass("active"); // set the active tab
+		            // get the data-rel value from selected tab and set as filter
+		            var $filter = $this.data("rel"); 
+		            // if we select view all, return to initial settings and show all
+		            $filter == 'all' ? 
+		                $(".fancybox")
+		                .attr("data-fancybox-group", "gallery")
+		                .not(":visible")
+		                .fadeIn() 
+		            : // otherwise
+		                $(".fancybox")
+		                .fadeOut(0)
+		                .filter(function () {
+		                    // set data-filter value as the data-rel value of selected tab
+		                    return $(this).data("filter") == $filter; 
+		                })
+		                // set data-fancybox-group and show filtered elements
+		                .attr("data-fancybox-group", $filter)
+		                .fadeIn(1000); 
+		        } // if
+		    }); // on
+		}); // ready
+    	</script>
+    	<!--texitimonial-->
+    	<script>
+
+
+   $('.quotes').quovolver({
+      equalHeight   : true
+    });
+
+    	</script>
+
+</body>
+</html>
